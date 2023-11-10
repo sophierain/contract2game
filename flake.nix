1cbcdbc49f73186d5a77898263f190a46dadda3f
David Terry <me@xwvvvvwx.com>
2023-11-10 15:51:47 +0100

    try out pyproject.nix

--- flake.nix
+++ flake.nix
@@ -3,37 +3,41 @@
 
   inputs = {
     nixpkgs.url = "github:nixos/nixpkgs/nixos-unstable";
+    pyproject.url = "github:nix-community/pyproject.nix";
+    pyproject.inputs.nixpkgs.follows = "nixpkgs";
     systems.url = "github:nix-systems/default";
+    flake-utils.url = "github:numtide/flake-utils";
+    flake-utils.inputs.systems.follows = "systems";
     act.url = "github:ethereum/act";
-    flake-utils = {
-      url = "github:numtide/flake-utils";
-      inputs.systems.follows = "systems";
-    };
   };
 
-  outputs = { self, flake-utils, nixpkgs, act, ... }:
+  outputs = { nixpkgs, pyproject, flake-utils, act, ... }:
     flake-utils.lib.eachDefaultSystem (system:
       let
+        inherit (nixpkgs) lib;
+
         pkgs = import nixpkgs { system = system; };
-        contract2game = pkgs.python3.pkgs.buildPythonApplication rec {
-          pname = "contract2game";
-          version = "0.0.1";
-          pyproject = true;
-          src = ./.;
-          propagatedBuildInputs = with pkgs.python3.pkgs; [ z3 setuptools ];
-          nativeBuildInputs = with pkgs.python3.pkgs; [
-            setuptools
-            wheel
-          ];
+        python = pkgs.python3;
+
+        # parse pyproject.toml
+        project = pyproject.lib.project.loadPyproject {
+          pyproject = lib.importTOML ./pyproject.toml;
         };
-      in rec {
-        packages.contract2game = contract2game;
-        packages.default = packages.contract2game;
-        devShell = pkgs.mkShell {
-          packages = [
-            act.packages.${system}.default
-            (pkgs.python3.withPackages (ps: with ps; [z3 setuptools]))
-          ];
-        };
+      in
+      {
+        devShells.default =
+          let
+            arg = pyproject.lib.renderers.withPackages { inherit python project; };
+            pythonEnv = python.withPackages arg;
+          in pkgs.mkShell {
+            packages = [
+              act.packages.${system}.default
+              pythonEnv
+            ];
+          };
+
+        packages.default =
+          let attrs = pyproject.lib.renderers.buildPythonPackage { inherit python project; };
+          in python.pkgs.buildPythonApplication attrs;
       });
 }
